9f4979e0a97d95bf8d733a0a5242a05a19e96afb
angelicahg <herrerangelik@gmail.com>
2023-05-09 17:44:39 -0500

    form in angular

--- src/app/components/formulariog/formulariog.component.ts
+++ src/app/components/formulariog/formulariog.component.ts
@@ -18,7 +18,7 @@ export class FormulariogComponent {
   constructor(private http: HttpClient ) { }
 
   onSubmit(): void {
-    const formData = { nombres: this.nombres, apellidos: this.apellidos };
+    const formData = { nombres: this.nombres, apellidos: this.apellidos,telefono: this.telefono,correo: this.correo,direccion: this.direccion,documento: this.documento };
     this.http.post('http://190.60.101.59:6003/api/Personas', formData)
       .subscribe((response) => {
         console.log(response);
